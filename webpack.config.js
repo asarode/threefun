01eebcf3b98b95f52a275c9e44e5e86c582a5bd4
arjun <sarodearjun57@gmail.com>
2016-03-20 17:07:17 -0700

    Setup

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    './src/index'
+  ],
+  output: {
+    path: __dirname,
+    filename: 'bundle.js',
+    publicPath: '/dist/'
+  },
+  resolve: {
+    extensions: ['', '.js']
+  },
+  devtool: 'eval-source-map',
+  plugins: [],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loaders: ['babel'],
+        include: path.join(__dirname, 'src')
+      }
+    ]
+  }
+};
